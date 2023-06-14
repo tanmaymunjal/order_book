dad85e93415f963d305b86e8eae8cadd29a3e5ea
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-06-14 21:15:08 +0530

    added current datetime functionality

--- src/login.rs
+++ src/login.rs
@@ -1,6 +1,27 @@
-use chrono;
+use chrono::{DateTime, Utc};
 
-pub fn login(sender_id:String,username:String, password:String,receiving_firm:String,receiving_target:String) -> String{
-    let login:String = format!("8=FIX.4.4|9=114| 35=A |34=1| 49={sender_id}| 52=20120927-13:15:34.754| 56={receiving_firm} |57={receiving_target}| 553={username}| 554={password}| 98=0 |108=30 |141=Y| 10=146|");
+pub fn login(
+    sender_id: String,
+    username: String,
+    password: String,
+    receiving_firm: String,
+    receiving_target: String,
+) -> String {
+    let current_time: String = get_current_datetime();
+    let login:String = format!("8=FIX.4.4|9=114| 35=A |34=1| 49={sender_id}| 52={current_time}| 56={receiving_firm} |57={receiving_target}| 553={username}| 554={password}| 98=0 |108=30 |141=Y| 10=146|");
     login
+}
+
+pub fn get_current_datetime() -> String {
+    let current_time: DateTime<Utc> = Utc::now();
+    format_time(current_time)
+}
+
+pub fn format_time(current_time: DateTime<Utc>) -> String {
+    // Parse the input string into a DateTime object
+    let parsed_dt = DateTime::parse_from_rfc3339(&current_time.to_string()).unwrap();
+
+    // Convert the parsed DateTime to the desired format
+    let formatted_dt = parsed_dt.format("%Y%m%d-%H:%M:%S%.3f").to_string();
+    formatted_dt
 }
